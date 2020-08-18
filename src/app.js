486d30bec3922b3932f79e75a92696710916391b
LeonardoCHb <leonardohably@hotmail.com>
2020-08-18 20:37:35 -0300

    adding put and get properties

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const { request, response } = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { v4: uuid } = require('uuid');
 
@@ -11,15 +13,44 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+    return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const {title, url, techs} = request.body
+    const likes = 0
+
+    const repository = {id: uuid(), title, url , techs,likes}
+
+    console.log(repository)
+
+    repositories.push(repository)
+
+    return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+    const { id } = request.params
+    const {title,url,techs} = request.body
+
+    const repositoryIndex= repositories.findIndex(repository => repository.id === id)
+
+    if(repositoryIndex < 0){
+      return response.status(400).json({ erro:"Repository not found!"})
+    }
+
+    const repository = {
+      id,
+      title,
+      url,
+      techs,
+    }
+
+    repositories[repositoryIndex]=repository
+    console.log(repository)
+
+    return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
